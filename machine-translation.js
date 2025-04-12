60534a7881b4faf738eef52f82c8770252c3bba4
Yusuke Kamiyamane <y@y14e.com>
2025-04-12 22:06:33 +0900

    20250412

--- machine-translation.js
+++ machine-translation.js
@@ -2,13 +2,13 @@ export function detectMachineTranslation() {
   const htmlElement = document.documentElement;
   const titleElement = document.getElementsByTagName('title')[0];
   const observer = new MutationObserver(() => {
-    if ([...htmlElement.classList].some(className => /translated-(ltr|rtl)/.test(className)) || titleElement.hasAttribute('_msttexthash')) {
+    if ([...htmlElement.classList].some(className => /translated-(ltr|rtl)/.test(className)) || htmlElement.getAttribute('lang') !== navigator.language || titleElement.hasAttribute('_msttexthash')) {
       window.dispatchEvent(new Event('machineTranslationDetected'));
       observer.disconnect();
     }
   });
   new Map([
-    [htmlElement, 'class'],
-    [titleElement, '_msttexthash'],
-  ]).forEach((attribute, element) => observer.observe(element, { attributeFilter: [attribute] }));
+    [htmlElement, ['class', 'lang']],
+    [titleElement, ['_msttexthash']],
+  ]).forEach((attributes, element) => observer.observe(element, { attributeFilter: attributes }));
 }
--- machine-translation.ts
+++ machine-translation.ts
@@ -2,13 +2,13 @@ export function detectMachineTranslation(): void {
   const htmlElement = document.documentElement;
   const titleElement = document.getElementsByTagName('title')[0];
   const observer = new MutationObserver(() => {
-    if ([...htmlElement.classList].some(className => /translated-(ltr|rtl)/.test(className)) || titleElement.hasAttribute('_msttexthash')) {
+    if ([...htmlElement.classList].some(className => /translated-(ltr|rtl)/.test(className)) || htmlElement.getAttribute('lang') !== navigator.language || titleElement.hasAttribute('_msttexthash')) {
       window.dispatchEvent(new Event('machineTranslationDetected'));
       observer.disconnect();
     }
   });
-  new Map<HTMLElement, string>([
-    [htmlElement, 'class'],
-    [titleElement, '_msttexthash'],
-  ]).forEach((attribute, element) => observer.observe(element, { attributeFilter: [attribute] }));
+  new Map<HTMLElement, string[]>([
+    [htmlElement, ['class', 'lang']],
+    [titleElement, ['_msttexthash']],
+  ]).forEach((attributes, element) => observer.observe(element, { attributeFilter: attributes }));
 }
