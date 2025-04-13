1b1aeaaf8034cbe01ae7078dedb9d2468c2fd406
Yusuke Kamiyamane <y@y14e.com>
2025-04-14 00:23:51 +0900

    20250414

--- machine-translation.js
+++ machine-translation.js
@@ -1,16 +1,30 @@
 export function detectMachineTranslation() {
   const htmlElement = document.documentElement;
   const titleElement = document.getElementsByTagName('title')[0];
+  const strategies = [
+    {
+      element: htmlElement,
+      attribute: 'class',
+      test: () => [...htmlElement.classList].some(className => /translated-(ltr|rtl)/.test(className)),
+    },
+    {
+      element: htmlElement,
+      attribute: 'lang',
+      test: () => htmlElement.getAttribute('lang') !== navigator.language,
+    },
+    {
+      element: titleElement,
+      attribute: '_msttexthash',
+      test: () => titleElement.hasAttribute('_msttexthash'),
+    },
+  ];
   const observer = new MutationObserver(() => {
-    if ([...htmlElement.classList].some(className => /translated-(ltr|rtl)/.test(className)) || htmlElement.getAttribute('lang') !== navigator.language || titleElement.hasAttribute('_msttexthash')) {
+    if (strategies.some(strategy => strategy.test())) {
       window.dispatchEvent(new Event('machineTranslationDetected'));
       observer.disconnect();
     }
   });
-  new Map([
-    [htmlElement, ['class', 'lang']],
-    [titleElement, ['_msttexthash']],
-  ]).forEach((attributes, element) => {
-    observer.observe(element, { attributeFilter: attributes });
+  strategies.forEach(({ element, attribute }) => {
+    observer.observe(element, { attributeFilter: [attribute] });
   });
 }
