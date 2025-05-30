a6efcab94d6d4a93f071fc026ac5443b49b34702
Yusuke Kamiyamane <y@y14e.com>
2025-05-30 10:36:56 +0900

    20250530

--- machine-translation.ts
+++ machine-translation.ts
@@ -25,6 +25,8 @@ export function detectMachineTranslation(): void {
     }
   });
   strategies.forEach(({ element, attribute }) => {
-    observer.observe(element, { attributeFilter: [attribute] });
+    observer.observe(element, {
+      attributeFilter: [attribute],
+    });
   });
 }
