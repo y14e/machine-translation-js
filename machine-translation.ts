a614d82615a4a06a01c382dcd33a6437e03dd833
Yusuke Kamiyamane <y@y14e.com>
2026-03-29 22:12:42 +0900

    Update machine-translation.ts

--- machine-translation.ts
+++ machine-translation.ts
@@ -1,4 +1,4 @@
-export function detectMachineTranslation(): () => {
+export function detectMachineTranslation(): () => void {
   const html = document.documentElement;
   const title = document.getElementsByTagName('title')[0];
   const strategies = [
