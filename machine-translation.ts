7c2ef8ca18e045477820ab475ddde12e168188b1
Yusuke Kamiyamane <y@y14e.com>
2026-04-04 12:18:31 +0900

    20260404

--- machine-translation.ts
+++ machine-translation.ts
@@ -3,17 +3,17 @@ export function detectMachineTranslation(): () => void {
   const title = document.getElementsByTagName('title')[0];
   const strategies = [
     {
-      attribute: 'class',
+      attr: 'class',
       element: html,
       test: () => [...html.classList].some((className) => /translated-(ltr|rtl)/.test(className)),
     },
     {
-      attribute: '_msttexthash',
+      attr: '_msttexthash',
       element: title,
       test: () => title.hasAttribute('_msttexthash'),
     },
     {
-      attribute: 'lang',
+      attr: 'lang',
       element: html,
       test: () => html.lang !== navigator.language,
     },
@@ -24,6 +24,6 @@ export function detectMachineTranslation(): () => void {
       observer.disconnect();
     }
   });
-  strategies.forEach(({ attribute, element }) => observer.observe(element, { attributeFilter: [attribute] }));
+  strategies.forEach(({ attr, element }) => observer.observe(element, { attributeFilter: [attr] }));
   return () => observer.disconnect();
 }
