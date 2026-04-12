79f9d5212295461e7c4769e0e63e97502605b435
Yusuke Kamiyamane <y@y14e.com>
2026-04-12 12:23:52 +0900

    Update machine-translation.ts

--- machine-translation.ts
+++ machine-translation.ts
@@ -1,46 +1,46 @@
 export function detectMachineTranslation(): () => void {
   const html = document.documentElement;
   const title = document.getElementsByTagName('title')[0];
+
   const strategies = [
     {
       attribute: 'class',
       element: html,
-      test: () => {
-        return [...html.classList].some((className) => {
-          return /translated-(ltr|rtl)/.test(className);
-        });
-      },
+      test: () =>
+        [...html.classList].some((className) =>
+          /translated-(ltr|rtl)/.test(className),
+        ),
     },
     {
       attribute: '_msttexthash',
       element: title,
-      test: () => {
-        return title.hasAttribute('_msttexthash');
-      },
+      test: () => title.hasAttribute('_msttexthash'),
     },
     {
       attribute: 'lang',
       element: html,
-      test: () => {
-        return new Intl.Locale(html.lang).language !== new Intl.Locale(navigator.language).language;
-      },
+      test: () =>
+        new Intl.Locale(html.lang).language !==
+        new Intl.Locale(navigator.language).language,
     },
   ];
+
   let observer: MutationObserver | null = new MutationObserver(() => {
-    if (
-      strategies.some((strategy) => {
-        return strategy.test();
-      })
-    ) {
+    if (strategies.some((strategy) => strategy.test())) {
       window.dispatchEvent(new Event('machineTranslationDetected'));
+
       observer?.disconnect();
       observer = null;
     }
   });
-  strategies.forEach(({ attribute, element }) => {
-    observer?.observe(element, { attributeFilter: [attribute] });
-  });
-  return () => {
+
+  for (const { attribute, element } of strategies) {
+    observer?.observe(element, {
+      attributeFilter: [attribute],
+    });
+  }
+
+  return (): void => {
     observer?.disconnect();
     observer = null;
   };
