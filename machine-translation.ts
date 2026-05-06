835f8a9019639c6bd2a120b34f3d976aab138e35
Yusuke Kamiyamane <y@y14e.com>
2026-05-06 12:39:03 +0900

    Update machine-translation.ts

--- machine-translation.ts
+++ machine-translation.ts
@@ -16,7 +16,7 @@ export function detectMachineTranslation() {
   const html = document.documentElement;
   const title = document.querySelector('title');
 
-  if (title === null) {
+  if (!title) {
     throw new Error('Missing <title> element');
   }
 
